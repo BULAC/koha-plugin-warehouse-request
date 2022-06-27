be20b13a200e7afa3b832906fca9e77e5a33270a
Nicolas Legrand <nicolas.legrand@bulac.fr>
2022-06-27 20:19:37 +0200

    Better UIX
    less clicks, better texts, less error possibilities

--- Koha/Plugin/Fr/UnivRennes2/WRM/intranet/receive.pl
+++ Koha/Plugin/Fr/UnivRennes2/WRM/intranet/receive.pl
@@ -36,17 +36,20 @@ my $query = CGI->new;
 
 #getting the template
 my ( $template, $librarian, $cookie, $flags ) = get_template_and_user(
-    {
-        template_name   => $template_name,
-        query           => $query,
-        type            => "intranet",
-        flagsrequired   => { circulate => "circulate_remaining_permissions" },
-    }
-    );
+                                                                      {
+                                                                       template_name   => $template_name,
+                                                                       query           => $query,
+                                                                       type            => "intranet",
+                                                                       flagsrequired   => { circulate => "circulate_remaining_permissions" },
+                                                                      }
+                                                                     );
 
 my $sessionID = $query->cookie("CGISESSID");
 my $session = get_session($sessionID);
 my $desk_id = C4::Context->userenv->{"desk_id"} // '';
+
+print $query->redirect("/cgi-bin/koha/circ/set-library.pl?referer=/receive.pl")
+  unless ($desk_id);
 
 my $desk = Koha::Desks->find($desk_id);
 my $error;
@@ -58,13 +61,14 @@ my $missing_barcode = $query->param("missing_barcode") // '';
 $missing_barcode =~ s/^\s*|\s*$//g;
 $missing_barcode = barcodedecode($missing_barcode) if $missing_barcode;
 
-unless ($missing_barcode =~ /^[0-9]{10}$/ or $missing_barcode =~ /^[0-9]{14}$/) {
-    $error = "$error, mauvais code barre : $missing_barcode";
-    $missing_barcode = ""
-}
-elsif ($missing_barcode =~ /^574/) {
-    $error = "$error, code barre de requête : $missing_barcode";
-    $missing_barcode = ""
+if ($missing_barcode ne '') {
+    unless ($missing_barcode =~ /^[0-9]{10}$/ or $missing_barcode =~ /^[0-9]{14}$/) {
+        $error = "$error, mauvais code barre : $missing_barcode";
+        $missing_barcode = ""
+    } elsif ($missing_barcode =~ /^574/) {
+        $error = "$error, code barre de requête : $missing_barcode";
+        $missing_barcode = ""
+    }
 }
 my $wrid    = $query->param("wrid") // '';
 my $op      = $query->param("op") // '';
@@ -75,8 +79,7 @@ my $op      = $query->param("op") // '';
 my $barcode_type;
 if ($barcode =~ /^574/) {
     $barcode_type = "stack_request" ;
-}
-elsif ($barcode) {
+} elsif ($barcode) {
     $barcode_type = "item" ;
 }
 
@@ -101,8 +104,7 @@ if ($barcode_type eq "item" and $op eq "confirm") {
     if ($@) {
         $error = "$error, $@";
     }
-}
-elsif ($barcode_type eq "stack_request" and $op eq "confirm") {
+} elsif ($barcode_type eq "stack_request" and $op eq "confirm") {
     $barcode =~ /^5740*([1-9][0-9]*)$/;
     my $wrid = $1;
     local $@;
@@ -121,38 +123,36 @@ elsif ($barcode_type eq "stack_request" and $op eq "confirm") {
     if ($@) {
         $error = "$error, $@";
     }
-}
-elsif (($barcode_type eq "item" or $missing_barcode) and ($op eq "confirmed" or $op eq "cancel") and $wrid >= 0) {
-        my $wr   = Koha::WarehouseRequests->find($wrid);
-        my $item = Koha::Items->find( $wr->itemnumber );
-        my $patron = Koha::Patrons->find($wr->borrowernumber);
-        $item->barcode($missing_barcode)->store()
-          if ($missing_barcode and ! $item->barcode);
-        $wr = $wr->complete();
-        my $resid = AddReserve({
-            branchcode       => $wr->borrower->branchcode,
-            borrowernumber   => $wr->borrower->borrowernumber,
-            biblionumber     => $wr->item->biblionumber,
-            priority         => 0,
-            reservation_date => output_pref({ dt => DateTime->now, dateformat => 'iso' , dateonly => 1 }),
-            resevenotes      => 'FROM_STACKS',
-            itemnumber       => $wr->item->itemnumber(),
-            found            => 'W',
-                                itemtype          => $wr->item->itype()
-                                desk_id => $desk_id
-                               });
-#        ModReserveAffect( $wr->item->itemnumber, $wr->borrower->borrowernumber, '', $resid, $desk_id);
-        my $res = Koha::Holds->find($resid);
+} elsif (($barcode_type eq "item" or $missing_barcode) and ($op eq "confirmed" or $op eq "cancel") and $wrid >= 0) {
+    my $wr   = Koha::WarehouseRequests->find($wrid);
+    my $item = Koha::Items->find( $wr->itemnumber );
+    my $patron = Koha::Patrons->find($wr->borrowernumber);
+    $item->barcode($missing_barcode)->store()
+      if ($missing_barcode and ! $item->barcode);
+    $wr = $wr->complete();
+    my $resid = AddReserve({
+                            branchcode       => $wr->borrower->branchcode,
+                            borrowernumber   => $wr->borrower->borrowernumber,
+                            biblionumber     => $wr->item->biblionumber,
+                            priority         => 0,
+                            reservation_date => output_pref({ dt => DateTime->now, dateformat => 'iso' , dateonly => 1 }),
+                            resevenotes      => 'FROM_STACKS',
+                            itemnumber       => $wr->item->itemnumber(),
+                            found            => 'W',
+                            itemtype         => $wr->item->itype(),
+                            desk_id          => $desk_id,
+                           });
+    #        ModReserveAffect( $wr->item->itemnumber, $wr->borrower->borrowernumber, '', $resid, $desk_id);
+    my $res = Koha::Holds->find($resid);
 
-        $template->param(
-                         item    => $item,
-                         reserve => $res,
-                         op      => $op,
-                         patron  => $patron,
-            )
-}
-elsif ($op eq "cancel" and $wrid >= 0) {
-    my $wr = Koha::WarehouseRequests->find($wrid);
+    $template->param(
+                     item    => $item,
+                     reserve => $res,
+                     op      => $op,
+                     patron  => $patron,
+                    )
+} elsif ($op eq "cancel" and $wrid >= 0) {
+    my $wr = Koha::WarehouseRequests->find( $wrid );
     my $item;
     $item = Koha::Items->find( $wr->itemnumber );
     my $patron = Koha::Patrons->find($wr->borrowernumber);
@@ -169,7 +169,7 @@ elsif ($op eq "cancel" and $wrid >= 0) {
                             itemtype          => $wr->item->itype(),
                             desk_id             => $desk_id,
                            });
-#    ModReserveAffect( $wr->item->itemnumber, $wr->borrower->borrowernumber, '', $resid, $desk_id);
+    #    ModReserveAffect( $wr->item->itemnumber, $wr->borrower->borrowernumber, '', $resid, $desk_id);
     my $res = Koha::Holds->find($resid);
 
     $template->param(
@@ -181,7 +181,7 @@ elsif ($op eq "cancel" and $wrid >= 0) {
                     )
 }
 
-$template->param(error => $error)
+$template->param(error => $error);
 
 
 output_html_with_http_headers $query, $cookie, $template->output;
