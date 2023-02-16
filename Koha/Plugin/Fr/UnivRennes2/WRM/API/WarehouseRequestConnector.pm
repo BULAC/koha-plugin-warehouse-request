984c5ef4d5992ac454e84be2d9e17465411df205
Nicolas Legrand <nicolas.legrand@bulac.fr>
2023-02-16 14:53:48 +0100

    Corrige un bug d'affichage
    les listes de documents réservés peuvent ne pas s'afficher sur le
    compte Opac quand un exemplaire est supprimé. Cela corrige le
    problème.

--- Koha/Plugin/Fr/UnivRennes2/WRM/API/WarehouseRequestConnector.pm
+++ Koha/Plugin/Fr/UnivRennes2/WRM/API/WarehouseRequestConnector.pm
@@ -624,14 +624,16 @@ sub _to_api {
         "title" => $biblio->title,
         "author" => $biblio->author
     };
-    $request->{item} = {
-        "holdingbranch" => $item->holding_branch->branchname,
-         #"location" => Koha::AuthorisedValues->find_by_koha_field( { kohafield => 'items.location', authorised_value => $item->location } )->lib,
-        "location" => $item->location,
-        "itemtype" => Koha::ItemTypes->find( $item->effective_itemtype )->description,
-        "itemcallnumber" => $item->itemcallnumber,
-        "barcode" => $item->barcode
-    };
+    if ($item) {
+        $request->{item} = {
+            "holdingbranch" => $item->holding_branch->branchname,
+                #"location" => Koha::AuthorisedValues->find_by_koha_field( { kohafield => 'items.location', authorised_value => $item->location } )->lib,
+                "location" => $item->location,
+                "itemtype" => Koha::ItemTypes->find( $item->effective_itemtype )->description,
+                "itemcallnumber" => $item->itemcallnumber,
+                "barcode" => $item->barcode
+        };
+    }
     if ($bystatus) {
         $request->{borrower} = {
             "firstname" => $borrower->firstname,
