a77552b4e9c745ca8b8ba4196b04cb31235b4cca
amaurygau <amaurygau@gmail.com>
2026-02-06 12:27:11 +0100

    Suppression de la subroutine updateWrongTransfer
    Disparition de la routine depuis la 24.11

--- Koha/Plugin/Fr/UnivRennes2/WRM/intranet/receive.pl
+++ Koha/Plugin/Fr/UnivRennes2/WRM/intranet/receive.pl
@@ -4,7 +4,7 @@ use Modern::Perl;
 
 use CGI qw ( -utf8 );
 use C4::Auth qw( get_template_and_user get_session haspermission );
-use C4::Circulation qw( barcodedecode GetBranchItemRule AddReturn updateWrongTransfer LostItem );
+use C4::Circulation qw( barcodedecode GetBranchItemRule AddReturn LostItem );
 use C4::Output qw(output_html_with_http_headers);
 use C4::Context;
 use Cwd qw(abs_path);
